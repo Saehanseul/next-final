abb1d90b1d1d197765c5e5c18c96ea90ffaa09b9
Saehanseul <saehanseul@gmail.com>
2024-03-28 19:58:49 +0900

    Feat: prefetch 추가

--- components/book-catetory.tsx
+++ components/book-catetory.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/book-category.module.css";
 
 export default function BookCategory({ display_name, list_name_encoded }) {
   return (
-    <Link href={`/list/${list_name_encoded}`}>
+    <Link href={`/list/${list_name_encoded}`} prefetch>
       <div className={styles.bookCategory}>
         <div>{display_name}</div>
       </div>
